ef7806591fc96309eb5853a7b0413188b00b0e72
andjmiskovic <andjelamiskovic2000@gmail.com>
2022-01-20 17:33:43 +0100

    added edit profile

--- WebProject/WebContent/js/profile.js
+++ WebProject/WebContent/js/profile.js
@@ -1,52 +1,89 @@
 function clear() {
-    $('.showing div').removeClass('visible');
-    $('.navbar label').removeClass('active');
+	$('.showing div').removeClass('visible');
+	$('.navbar label').removeClass('active');
 }
 
 $('#photos').click(function() {
-    clear();
-    $(this).addClass('active');
-    $('.photos').addClass('visible');
+	clear();
+	$(this).addClass('active');
+	$('.photos').addClass('visible');
 });
 
 $('#posts').click(function() {
-    clear();
-    $(this).addClass('active');
-    $('.posts').addClass('visible');
+	clear();
+	$(this).addClass('active');
+	$('.posts').addClass('visible');
 });
 
 $('#friends').click(function() {
-    clear();
-    $(this).addClass('active');
-    $('.friends').addClass('visible');
+	clear();
+	$(this).addClass('active');
+	$('.friends').addClass('visible');
 });
+
+$(document).ready(function() {
+	$.ajax({
+		url: "rest/profile/",
+		type: "GET",
+		contentType: "application/json",
+		dataType: "json",
+		complete: function(data) {
+			var user = JSON.parse(data.responseText)
+			console.log(user)
+			$("#profile-user-name").html("@" + user.username);
+			$("#number-of-posts").html(user.posts.length + " Posts");
+			$("#number-of-photos").html(user.posts.length + " Photos");			// TODO
+			$("#number-of-friends").html(user.friends.length + " Friends");		// kada je nula izbaci 1 ?
+			$("#date-of-birth").html(user.dateOfBirth);
+			$("#profile-bio-text").html(user.biography);
+		}
+	});
+})
+
 
 // friendship filter
 $(document).ready(function() {
-    $("#friends-search").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $(".friendship h3").filter(function() {
-            $(this).parent().parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    });
+	$("#friends-search").on("keyup", function() {
+		var value = $(this).val().toLowerCase();
+		$(".friendship h3").filter(function() {
+			$(this).parent().parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
 });
 
 function goToHomepage() {
-    window.location.href = "feed.html";
+	window.location.href = "feed.html";
 }
 
 function logOut() {
-    window.location.href = "index.html";
+	window.location.href = "index.html";
 }
 
+
 $("#edit-profile").click(function() {
-    $(".edit-profile-card").fadeIn();
+	$(".edit-profile-card").fadeIn();
+	$.ajax({
+		url: "rest/profile/",
+		type: "GET",
+		contentType: "application/json",
+		dataType: "json",
+		complete: function(data) {
+			var user = JSON.parse(data.responseText)
+			console.log(user)
+			$("#name").val(user.name);
+			$("#surname").val(user.surname);
+			$("#dateOfBirth").val(user.dateOfBirth);
+			$("#biography").val(user.biography);
+		}
+	});
+
+
 })
 
 $(".cancel-btn").click(function() {
-    $(".edit-profile-card").fadeOut();
+	$(".edit-profile-card").fadeOut();
 })
 
 $("#cancel-edit").click(function() {
-    $(".edit-profile-card").fadeOut();
+	$(".edit-profile-card").fadeOut();
 })
